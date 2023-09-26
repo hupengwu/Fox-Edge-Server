ee71802f6ef55e10ab2bcb16562940d674a87a12
hupengwu <5389408@qq.com>
2023-09-26 14:47:34 +0800

    解决线程死循环，带来的CPU占用率飙升问题

--- fox-edge-server-proxy/fox-edge-server-proxy-cloud/publisher/fox-edge-server-proxy-publisher-record/src/main/java/cn/foxtech/proxy/cloud/publisher/RecordEntityManageScheduler.java
+++ fox-edge-server-proxy/fox-edge-server-proxy-cloud/publisher/fox-edge-server-proxy-publisher-record/src/main/java/cn/foxtech/proxy/cloud/publisher/RecordEntityManageScheduler.java
@@ -31,6 +31,8 @@ public class RecordEntityManageScheduler extends PeriodTaskService {
 
     @Override
     public void execute(long threadId) throws Exception {
+        Thread.sleep(10 * 1000);
+
         // 云端是否处于锁定状态
         if (!this.remoteService.isLogin() && this.remoteService.isLockdown()) {
             return;
