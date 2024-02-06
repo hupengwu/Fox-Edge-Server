3c0fb5435c7b95b2a6ceb624ce057d9b4dea64e5
hupengwu <5389408@qq.com>
2024-02-06 15:44:19 +0800

    减少mybatis数据库连接池的连接，减少内存占用率

--- fox-edge-server-controller/fox-edge-server-controller-service/src/main/java/cn/foxtech/controller/service/ControllerServiceApplication.java
+++ fox-edge-server-controller/fox-edge-server-controller-service/src/main/java/cn/foxtech/controller/service/ControllerServiceApplication.java
@@ -2,8 +2,9 @@ package cn.foxtech.controller.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ControllerServiceApplication {
 
     public static void main(String[] args) {
--- fox-edge-server-kernel/fox-edge-server-kernel-gateway/src/main/java/cn/foxtech/kernel/gateway/config/EntityConfig.java
+++ fox-edge-server-kernel/fox-edge-server-kernel-gateway/src/main/java/cn/foxtech/kernel/gateway/config/EntityConfig.java
@@ -1,7 +1,6 @@
 package cn.foxtech.kernel.gateway.config;
 
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * 告知Spring框架去扫描其他包中的Component
  */
 @Configuration
-@ComponentScan(basePackages = { "cn.foxtech.common.entity.manager"})
+@ComponentScan(basePackages = {"cn.foxtech.common.entity.manager"})
 public class EntityConfig {
 }
 
